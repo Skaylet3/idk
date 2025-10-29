8b142071e5021ccd55937b97cc7efb0f6efbf6fd
Efraim Barkai <skaylet2007@gmail.com>
2025-10-29 13:04:40 +0100

    fix bug

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: false,
+  // },
 };
 
 export default nextConfig;
